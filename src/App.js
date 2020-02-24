43317fd9f5bb0e9beeaa89dadf17ba3da78cb848
Beni <beninigmal@gmail.com>
2020-02-24 18:28:15 -0300

    estudado até a aula 7. Entender melhor RESTful.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import MenuSuperior from './components/MenuSuperior/MenuSuperior';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button className="btn">Teste</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    
+    render(){
+      return(
+      <div>
+        <MenuSuperior/>
+      </div>
+      );
+    }
 }
 
 export default App;
